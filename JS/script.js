4464ecfac3b288d3a9672919591af331cfe3912a
abdiali2021 <abdulkadir.abdullahi@student.moringaschool.com>
2021-11-12 19:06:27 +0300

    add js functions

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+function fn() {
+    var d= newDate();
+
+
+
+var date=parseInt(document.getElementById("day").value);
+var month=parseInt(document.getElementById("month").value);
+var year=parseInt(document.getElementById("year").value);
+
+if (day <0 || day>31 || month<0 || month>12) {
+    alert("Enter valid date")
+}
+}
+
+
+
+
+
