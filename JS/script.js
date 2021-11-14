718160fd1146cc99ecb1e05ad3cb811243307783
abdiali2021 <abdulkadir.abdullahi@student.moringaschool.com>
2021-11-14 16:51:47 +0300

    change functionality

--- JS/script.js
+++ JS/script.js
@@ -1,84 +1,30 @@
-function fn() {
-    var d= newDate();
+const form = document.querySelector('.form')
 
+const maleNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+]
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-var date=parseInt(document.getElementById("day").value);
-var month=parseInt(document.getElementById("month").value);
-var year=parseInt(document.getElementById("year").value);
+    const akanName = document.querySelector('.akanName')
 
-if (day <0 || day>31 ) {
-    document.getElementsByClassName('dateerror').innerHtml= "Input a valid date";
-    return false;
-}else if(date == "") {
-    document.getElementsByClassName("dateerror").innerHtml="Field is empty";
-    return false;
-}else {
-    d.setDate(date);
-}
+    const birthdate = document.querySelector('.dateOfBirth').value
+    const day = new Date(birthdate).getDay()
 
-if (month <0 || month>12 ) {
-    document.getElementsByClassName('montherror').innerHtml= "Input a valid date";
-    return false;
-}else if(month == "") {
-    document.getElementsByClassName("dateerror").innerHtml="Field is empty";
-    return false;
-}else {
-    d.setMonth(month);
-}
-
-if (year <0) {
-    document.getElementsByClassName('yearerror').innerHtml= "Input a valid date";
-    return false;
-}else if(year == "") {
-    document.getElementsByClassName("yearerror").innerHtml="Field is empty";
-    return false;
-}else {
-    d.setYear(year);
-}
-
-var day= d.getDay();
-var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday",]
-var male = ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame",]
-var female = ["akosua", "adwoa", "abenaa", "akua", "yaa", "afua", "ama"]
-
-
-function findAkan() {
-    var ismale = document.getElementById("male"),checked;
-    var isfemale = document.getElementById("female").checked;
-    if (ismale == true && isfemale == false) {
-        if (day == 0) {
-            alert("your name is " + male(0));
-         }
-         else if (day ==1) {
-         alert("your name is" + male(1));
-         }
-         else if(day ==2) {
-         alert("your name is" + male_(2));
-         }
-         else if(day ==3) {
-            alert("your name is" + male_(3));
-            }
-            else if(day ==4 {
-                alert("your name is" + male_(4));
-                }
-                else if(day ==5) {
-                    alert("your name is" + male_(5));
-                    }    
-
-             else if(day ==6) {
-               alert("your name is" + male_(6));
-              }
-    
-     }
-}
-
-findAkan();
-
-
-}
-
-
-
-
-
+    if (day) {
+        if (document.getElementById('male')) {
+            akanName.textContent = `Your Akan name is ${maleNames[day]}`
+        }
+        if (document.getElementById('female')) {
+            akanName.textContent = `Your Akan name is ${femaleNames[day]}`
+        }
+    }
+})
